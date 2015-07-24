c3f818fb45ea55da566cd5c4cb51c6ab5ebd347a
weclapp <weclapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-24 10:34:41 +0000

    Code cleaning
    git-svn-id: https://plugins.svn.wordpress.org/weclapp/tags/1.1@1205655 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/weclapp.js
+++ js/weclapp.js
@@ -1,8 +1,8 @@
 function registerUser( campaignType ) {
-	// get the form data
-	// there are many ways to get this data using jQuery ( you can use the class or id also )
 
+	//create an array for all selected campaigns
 	campaignIds = [];
+	//fill that array 
 	jQuery( 'input[name=checkbox_' + campaignType + '][value=1]' ).each( function( index, elem ){
 		campaignIds.push( jQuery( elem ).data( 'weclapp-campaign-id' ));
 	} );
@@ -14,54 +14,58 @@ function registerUser( campaignType ) {
 		'phone'    			: jQuery( 'input[name=wc_phone_'  + campaignType + ']' ).val(),
 		'campaignIds' 		: json_campaignIds,
 	};
+	//hide all message boxes and remove the red border, if existing
 	jQuery( '#errorbox_' + campaignType).hide();
 	jQuery( '#successbox_' + campaignType).hide();
 	jQuery( '.form-group' ).removeClass( 'has-error' ); // remove the error class
+	//empty error box
 	jQuery( '#errorbox_' + campaignType + ' span' ).empty();
+	//show loader animation
 	jQuery('#loader_' + campaignType).show();
 	jQuery.ajax( {
-		method        : 'POST', // define the type of HTTP verb we want to use ( POST for our form )
+		method        : 'POST', 
 		url         : frontendajax.ajaxurl,
-		data        : formData, // our data object
+		data        : formData, 
 		processData : true,
 		success: function( data ) {
 		data = JSON.parse( data );
+			//hide the loader animation
 			jQuery('#loader_' + campaignType).hide();
 			 if ( data.errors ) {
+				//show errorbox for displaying errors
 				jQuery( '#errorbox_'  + campaignType ).show();
-				// handle errors for name ---------------
+				// display errors for name
 				if ( data.errors.name ) {
 					jQuery( '#name-group_'  + campaignType ).addClass( 'has-error' ); // add the error class to show red input
-					jQuery( '#errorbox_'  + campaignType  + ' span' ).append( data.errors.name ); // add the actual error message input
+					jQuery( '#errorbox_'  + campaignType  + ' span' ).append( data.errors.name ); // add the error message 
 				}
 
-				// handle errors for email ---------------
+				// display errors for email 
 				if (  data.errors.email ) {
 					jQuery( '#email-group_'  + campaignType ).addClass( 'has-error' ); // add the error class to show red input
-					jQuery( '#errorbox_'  + campaignType  + ' span' ).append( data.errors.email ); // add the actual error message under input
+					jQuery( '#errorbox_'  + campaignType  + ' span' ).append( data.errors.email ); // add the error message 
 				}
-				// handle errors for phone ---------------
+				// display errors for phone 
 				if ( data.errors.phone ) {
 					jQuery( '#phone-group_'  + campaignType ).addClass( 'has-error' ); // add the error class to show red input
-					jQuery( '#errorbox_'  + campaignType  + ' span' ).append( data.errors.phone );
+					jQuery( '#errorbox_'  + campaignType  + ' span' ).append( data.errors.phone ); // add the error message 
 				}
-				// handle errors for campaign ---------------
+				// display errors for campaign
 				if ( data.errors.campaignIds ) {
-					jQuery( '#errorbox_'  + campaignType + ' span' ).append( data.errors.campaignIds );
+					jQuery( '#errorbox_'  + campaignType + ' span' ).append( data.errors.campaignIds ); // add the error message 
 				}
+				// display wordpress errors
 				if ( data.errors.wp_remote_request ) {
-					jQuery( '#errorbox_'  + campaignType  + ' span' ).append( data.errors.wp_remote_request );
+					jQuery( '#errorbox_'  + campaignType  + ' span' ).append( data.errors.wp_remote_request ); // add the error message 
 				}
+				// display weclapp errors
 				if ( data.errors.weclappApi ) {
-					jQuery( '#errorbox_'  + campaignType  + ' span' ).append( data.errors.weclappApi );
+					jQuery( '#errorbox_'  + campaignType  + ' span' ).append( data.errors.weclappApi ); // add the error message 
 				}
 			} else {
-				// ALL GOOD! just show the success message!
-				//if (  !jQuery.isEmptyObject(  )  )
-				//{
+					//show success message
 					jQuery( '#successbox_'  + campaignType  + ' span' ).html( data.message );
 					jQuery( '#successbox_'  + campaignType).show();
-				//}
 			  }
 		}
 	} ).fail( handleError );
@@ -73,37 +77,24 @@ function handleError( error ){
 	console.log( error );
 }
 
-jQuery( document ).ready( function(  ) {		
-	  jQuery( ".webinar-container .webinar-checkbox" ).on(  "click", function( event ) {
+jQuery( document ).ready( function(  ) {	
 
-	  if ( jQuery( this ).find( 'input[type="hidden"]' ).val(  ) != 0 ) {
-		jQuery( this ).removeClass( "active" );
-		jQuery( this ).find( 'input[type="hidden"]' ).val( 1 );
-	  } else {
-		jQuery( this ).addClass( "active" );
-		jQuery( this ).find( 'input[type="hidden"]' ).val( 0 );
-	  }
-	  event.preventDefault(  );
-	  event.stopPropagation(  );
-
-	} );
-
+	//display campaign description on header-click
 	jQuery( ".webinar-container .webinar-head" ).on(  "click", function(  event  ) {
-	  jQuery( this ).parent( ".webinar-box" ).toggleClass( "active" ).find( ".webinar-content" ).slideToggle(  );
-	  jQuery( this ).parent( ".webinar-box" ).siblings( ".active" ).removeClass( "active" ).find( ".webinar-content" ).slideUp(  );
+	    jQuery( this ).parent( ".webinar-box" ).toggleClass( "active" ).find( ".webinar-content" ).slideToggle();
+	    jQuery( this ).parent( ".webinar-box" ).siblings( ".active" ).removeClass( "active" ).find( ".webinar-content" ).slideUp();
 	} );
 
-
-	jQuery( ".webinar-container .webinar-checkbox" ).on(  "click", function( event ) {
-	  event.preventDefault(  );
-	  event.stopPropagation(  );
-	  if ( jQuery( this ).find( 'input[type="hidden"]' ).val(  ) == 0 ) {
-		jQuery( this ).addClass( "active" );
-		jQuery( this ).find( 'input[type="hidden"]' ).val( 1 );
-	  } else {
-		jQuery( this ).removeClass( "active" );
-		jQuery( this ).find( 'input[type="hidden"]' ).val( 0 );
-	  }
-
+	//change checkbox image on click
+	jQuery( ".webinar-container .webinar-checkbox" ).on( "click", function( event ) {
+		event.preventDefault();
+		event.stopPropagation();
+		if ( jQuery( this ).find( 'input[type="hidden"]' ).val(  ) == 0 ) {
+			jQuery( this ).addClass( "active" );
+			jQuery( this ).find( 'input[type="hidden"]' ).val( 1 );
+		} else {
+			jQuery( this ).removeClass( "active" );
+			jQuery( this ).find( 'input[type="hidden"]' ).val( 0 );
+		}
 	} );
 } ); 
